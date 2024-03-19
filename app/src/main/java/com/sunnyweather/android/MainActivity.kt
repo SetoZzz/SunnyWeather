d043911950c62338701f4b59298522e69ab07fb6
SetoZzz <1440711277@qq.com>
2024-03-19 09:51:34 +0800

    实现搜索全球城市数据功能

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -17,31 +17,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btn:Button = findViewById(R.id.button)
+//        val btn:Button = findViewById(R.id.)
 
-        btn.setOnClickListener{
-            val retrofit = Retrofit.Builder()
-                .baseUrl("http://10.0.2.2/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-            val appService = retrofit.create(AppService::class.java)
-            appService.getAppData().enqueue(object : Callback<List<App>>{
-                override fun onResponse(call: Call<List<App>>, response: Response<List<App>>) {
-                    val list = response.body()
-                    if (list != null){
-                        for (app in list){
-                            Log.d("MainActivity", "id is ${app.id}")
-                            Log.d("MainActivity", "name is ${app.name}")
-                            Log.d("MainActivity", "version is ${app.version}")
-                        }
-                    }
-                }
-
-                override fun onFailure(call: Call<List<App>>, t: Throwable) {
-                    t.printStackTrace()
-                }
-
-            })
-        }
+//        btn.setOnClickListener{
+//            val retrofit = Retrofit.Builder()
+//                .baseUrl("http://10.0.2.2/")
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build()
+//            val appService = retrofit.create(AppService::class.java)
+//            appService.getAppData().enqueue(object : Callback<List<App>>{
+//                override fun onResponse(call: Call<List<App>>, response: Response<List<App>>) {
+//                    val list = response.body()
+//                    if (list != null){
+//                        for (app in list){
+//                            Log.d("MainActivity", "id is ${app.id}")
+//                            Log.d("MainActivity", "name is ${app.name}")
+//                            Log.d("MainActivity", "version is ${app.version}")
+//                        }
+//                    }
+//                }
+//
+//                override fun onFailure(call: Call<List<App>>, t: Throwable) {
+//                    t.printStackTrace()
+//                }
+//
+//            })
+//        }
     }
 }
